1f6c935882252326e64cf29acb42c8dd4385b9c7
BasharNader <bashar.nader@outlook.com>
2021-01-14 12:21:56 +0330

    connect to state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import './App.css';
-import City from './components/City';
-import Form from './components/Form';
-import Stats from './components/Stats';
-import MainStat from './components/MainStat';
+import "./App.css";
+import City from "./components/City";
+import Form from "./components/Form";
+import Stats from "./components/Stats";
+import MainStat from "./components/MainStat";
+import { useState } from "react";
 
 function App() {
-  const stats = [
-    { title: 'title1', value: 'value1' },
-    { title: 'title2', value: 'value1' },
-    { title: 'title3', value: 'value1' },
-    { title: 'title4', value: 'value1' }
-  ];
+  const [state, setState] = useState("");
+  const [desc, setDesc] = useState("");
+  const [city, setCity] = useState("");
+
+  const [stats, setStats] = useState([
+    { title: "title1", value: "value1" },
+    { title: "title2", value: "value1" },
+    { title: "title3", value: "value1" },
+    { title: "title4", value: "value1" },
+  ]);
+
+  
   return (
     <div className="App">
       <h2>Open Weather App</h2>
       <Form />
-      <MainStat state="Clouds" desc="Cloudy"/>
-      <City name="Rasht" />
+      <MainStat state={state} desc={desc} />
+      <City name={city} />
       <Stats stats={stats} />
     </div>
   );
