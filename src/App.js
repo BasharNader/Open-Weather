88d1e0caa653cbcc96e4acf8ccd217d3481c2680
BasharNader <bashar.nader@outlook.com>
2021-01-14 12:02:16 +0330

    App and City Title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import City from './components/City';
 import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
+      <h2>Open Weather App</h2>
       <Form />
+      <City name="Rasht" />
     </div>
   );
 }
---
+++ src/components/City.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const City = ({ name }) => {
+  return <h3 className="city">City: {name}</h3>;
+};
+
+export default City;
