4aa40c75abe8e2264aa66e57d2d776e9437de4e1
BasharNader <bashar.nader@outlook.com>
2021-01-14 13:54:37 +0330

    ui changes

--- src/components/StatItem.js
+++ src/components/StatItem.js
@@ -4,11 +4,23 @@ const StatItem = (props) => {
   const title = props.title;
   const value = props.value;
 
-
-  return <div className="StatItem">
-    <h5 className="stat-title">{title}</h5>
-    {title === "Icon" ? <img src={`http://openweathermap.org/img/wn/${value}@2x.png`} alt="icon"/> : <p className="stat-value">{value}</p>}
-  </div>;
+  return (
+    <div className="StatItem">
+      {title === "Icon" ? (
+        <img
+          src={`http://openweathermap.org/img/wn/${value}@2x.png`}
+          className="icon-img"
+          alt="icon"
+        />
+      ) : (
+        <>
+          {" "}
+          <h5 className="stat-title">{title}</h5>
+          <p className="stat-value">{value}</p>{" "}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default StatItem;
